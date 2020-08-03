f1dc6cfba443ec04a4b680513c3914f82a36db0a
Aloïs Micard <lunamicard@gmail.com>
2020-08-04 00:21:07 +0200

    Basic map system

--- src/main.c
+++ src/main.c
@@ -1,4 +1,50 @@
+#include <stdlib.h>
 #include <ncurses.h>
+
+struct Map
+{
+    char *tiles;
+    unsigned short total_row;
+    unsigned short total_column;
+} typedef Map;
+
+Map *map_generate(int row, int column)
+{
+    Map *map = malloc(sizeof(Map));
+    map->tiles = malloc(sizeof(char) * row * column);
+
+    // Generate basic terrain
+    for (int x = 0; x < row; x++) {
+        for (int y = 0; y < column; y++) {
+            int pos = x * column + y;
+            map->tiles[pos] = 'A';
+        }
+    }
+
+    map->tiles[0] = 'M';
+    map->tiles[1] = 'C';
+
+    map->total_row = row;
+    map->total_column = column;
+
+    return map;
+}
+
+void map_draw(Map *map)
+{
+    for (int x = 0; x < map->total_row; x++) {
+        for (int y = 0; y < map->total_column; y++) {
+            int pos = x * map->total_column + y;
+            mvaddch(x, y, map->tiles[pos]);
+        }
+    }
+}
+
+void map_free(Map *map)
+{
+    free(map->tiles);
+    free(map);
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,27 +54,20 @@ int main(int argc, char *argv[])
     // Enable color support
     start_color();
 
-    // Create default color pair
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    // Generate a basic map
+    Map *map = map_generate(LINES, COLS);
 
-    // Bind color attributes
-    attron(COLOR_PAIR(1));
-
-    // Draw something
-    for (int x = 0; x < COLS; x++) {
-        for (int y = 0; y < LINES; y++) {
-            mvaddch(y, x, 'A');
-        }
-    }
-
-    // Disable color attributes
-    attroff(COLOR_PAIR(1));
+    // Draw the map as-is
+    map_draw(map);
 
     // Print it on to the real screen
     refresh();
 
     // Wait for user input
     getch();
+
+    // Free the map
+    map_free(map);
 
     // End ncurses mode
     endwin();
